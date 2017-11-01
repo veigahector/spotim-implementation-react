d21683ba5b3ea436818a8ffac9f8cf1b7bd4a9c5
Max <max@spot.im>
2017-11-01 16:56:33 -0400

    prettier code

--- SpotIMConversation.jsx
+++ SpotIMConversation.jsx
@@ -1,88 +1,91 @@
 class SpotIMConversation extends React.Component {
-	static displayName = 'SpotIMConversation'
-	static defaultProps = {
-		messagesCount: null,
-		enableSeo: false
-	};
-	constructor() {
-		super();
-		this.container = null;
-	}
-	componentDidMount() {
-		this.initConversation(this.props);
-	}
-	initConversation(props) {
-		var {
-			spotId,
-			postId,
-			canonicalUrl,
-			disqusIdentifier,
-			disqusUrl,
-			enableSeo,
-			messagesCount
-		} = props;
-		if (!this.container) {
-			console.warn('SpotIMConversation: missing container');
-			return;
-		}
-		if (!props.spotId) {
-			console.warn('SpotIMConversation: missing Spot ID');
-			return;
-		}
-		if (!props.postId) {
-			console.warn('SpotIMConversation: missing Post ID');
-			return;
-		}
-		if (!window || !window.document) {
-			console.warn('SpotIMConversation: this component supports client only');
-			return;
-		}
-		if (!canonicalUrl) {
-			canonicalUrl = window.location.href;
-		}
-		var launcherScript = document.createElement('script');
-		launcherScript.setAttribute('async', 'async');
-		launcherScript.setAttribute('src', 'https://launcher.spot.im/spot/' + spotId);
-		launcherScript.setAttribute('data-spotim-module', 'spotim-launcher');
-		launcherScript.setAttribute('data-post-id', postId);
-		launcherScript.setAttribute('data-post-url', canonicalUrl);
-		launcherScript.setAttribute('data-facebook-url', canonicalUrl);
-		if (disqusIdentifier) {
-			launcherScript.setAttribute('data-disqus-identifier', disqusIdentifier);
-		}
-		if (disqusUrl) {
-			launcherScript.setAttribute('data-disqus-url', disqusUrl);
-		}
-		if (enableSeo) {
-			launcherScript.setAttribute('data-seo-enabled', enableSeo);
-		}
-		if (messagesCount) {
-			launcherScript.setAttribute('data-messages-count', messagesCount);
-		}
-		this.container.appendChild(launcherScript);
-	}
-	destroyConversation() {
-		if (!this.container) {
-			console.warn('SpotIMConversation: missing container');
-			return false;
-		}
-		this.container.innerHTML = '';
-		return true;
-	}
-	componentWillReceiveProps(nextProps) {
-		var { spotId: currentSpotId, postId: currentPostId } = this.props;
-		var { spotId: nextSpotId, postId: nextPostId } = nextProps;
-		if (currentSpotId !== nextSpotId || currentPostId !== nextPostId) {
-			var destroyed = this.destroyConversation();
-			if (destroyed) {
-				this.initConversation(nextProps);
-			}
-		}
-	}
-	shouldComponentUpdate() {
-		return false;
-	}
-	render() {
-		return <div ref={el => this.container = el}></div>;
-	}
+  static displayName = 'SpotIMConversation';
+  static defaultProps = {
+    messagesCount: null,
+    enableSeo: false,
+  };
+  constructor() {
+    super();
+    this.container = null;
+  }
+  componentDidMount() {
+    this.initConversation(this.props);
+  }
+  initConversation(props) {
+    var {
+      spotId,
+      postId,
+      canonicalUrl,
+      disqusIdentifier,
+      disqusUrl,
+      enableSeo,
+      messagesCount,
+    } = props;
+    if (!this.container) {
+      console.warn('SpotIMConversation: missing container');
+      return;
+    }
+    if (!props.spotId) {
+      console.warn('SpotIMConversation: missing Spot ID');
+      return;
+    }
+    if (!props.postId) {
+      console.warn('SpotIMConversation: missing Post ID');
+      return;
+    }
+    if (!window || !window.document) {
+      console.warn('SpotIMConversation: this component supports client only');
+      return;
+    }
+    if (!canonicalUrl) {
+      canonicalUrl = window.location.href;
+    }
+    var launcherScript = document.createElement('script');
+    launcherScript.setAttribute('async', 'async');
+    launcherScript.setAttribute(
+      'src',
+      'https://launcher.spot.im/spot/' + spotId
+    );
+    launcherScript.setAttribute('data-spotim-module', 'spotim-launcher');
+    launcherScript.setAttribute('data-post-id', postId);
+    launcherScript.setAttribute('data-post-url', canonicalUrl);
+    launcherScript.setAttribute('data-facebook-url', canonicalUrl);
+    if (disqusIdentifier) {
+      launcherScript.setAttribute('data-disqus-identifier', disqusIdentifier);
+    }
+    if (disqusUrl) {
+      launcherScript.setAttribute('data-disqus-url', disqusUrl);
+    }
+    if (enableSeo) {
+      launcherScript.setAttribute('data-seo-enabled', enableSeo);
+    }
+    if (messagesCount) {
+      launcherScript.setAttribute('data-messages-count', messagesCount);
+    }
+    this.container.appendChild(launcherScript);
+  }
+  destroyConversation() {
+    if (!this.container) {
+      console.warn('SpotIMConversation: missing container');
+      return false;
+    }
+    this.container.innerHTML = '';
+    return true;
+  }
+  componentWillReceiveProps(nextProps) {
+    var {spotId: currentSpotId, postId: currentPostId} = this.props;
+    var {spotId: nextSpotId, postId: nextPostId} = nextProps;
+    if (currentSpotId !== nextSpotId || currentPostId !== nextPostId) {
+      var destroyed = this.destroyConversation();
+      if (destroyed) {
+        this.initConversation(nextProps);
+      }
+    }
+  }
+  shouldComponentUpdate() {
+    return false;
+  }
+  render() {
+    return <div ref={el => (this.container = el)} />;
+  }
 }
--- SpotIMPopularInTheCommunity.jsx
+++ SpotIMPopularInTheCommunity.jsx
@@ -1,38 +1,43 @@
 class SpotIMPopularInTheCommunity extends React.Component {
-	constructor() {
-		super();
-		this.container = null;
-	}
-	componentDidMount() {
-		if (!this.container) {
-			console.warn('SpotIMPopularInTheCommunity: missing container');
-			return;
-		}
-		if (!this.props.spotId) {
-			console.warn('SpotIMPopularInTheCommunity: missing Spot ID');
-			return;
-		}
-		if (!window || !window.document) {
-			console.warn('SpotIMConversation: this component supports client only');
-			return;
-		}
-		this.initPopularInTheCommunity();
-	}
-	initPopularInTheCommunity() {
-		var { spotId } = this.props;
-		var script = document.createElement('script');
-		script.setAttribute('async', 'async');
-		script.setAttribute('src', 'https://recirculation.spot.im/spot/' + spotId);
-		this.container.appendChild(script);
-	}
-	shouldComponentUpdate() {
-		return false;
-	}
-	render() {
-		return (
-			<div ref={ el => this.container = el }>
-				<div data-spotim-module="recirculation" data-spot-id={ this.props.spotId }></div>
-			</div>
-		);
-	}
+  constructor() {
+    super();
+    this.container = null;
+  }
+  componentDidMount() {
+    if (!this.container) {
+      console.warn('SpotIMPopularInTheCommunity: missing container');
+      return;
+    }
+    if (!this.props.spotId) {
+      console.warn('SpotIMPopularInTheCommunity: missing Spot ID');
+      return;
+    }
+    if (!window || !window.document) {
+      console.warn(
+        'SpotIMPopularInTheCommunity: this component supports client only'
+      );
+      return;
+    }
+    this.initPopularInTheCommunity();
+  }
+  initPopularInTheCommunity() {
+    var {spotId} = this.props;
+    var script = document.createElement('script');
+    script.setAttribute('async', 'async');
+    script.setAttribute('src', 'https://recirculation.spot.im/spot/' + spotId);
+    this.container.appendChild(script);
+  }
+  shouldComponentUpdate() {
+    return false;
+  }
+  render() {
+    return (
+      <div ref={el => (this.container = el)}>
+        <div
+          data-spotim-module="recirculation"
+          data-spot-id={this.props.spotId}
+        />
+      </div>
+    );
+  }
 }
